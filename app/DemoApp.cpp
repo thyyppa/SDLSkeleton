2e961c994969b58c783079c3db495a92887ddb75
Travis Hyyppä <travishyyppa@gmail.com>
2017-03-30 10:35:28 -0600

    Color demo box every loop!

--- app/DemoApp.cpp
+++ app/DemoApp.cpp
@@ -7,13 +7,13 @@ int DemoApp::run()
      */
 
     SDL_Rect box = { WIDTH / 2, HEIGHT / 2, 10, 10 };
-    SDL_SetRenderDrawColor( renderer, 0xff, 0xff, 0xff, 0xff );
 
     /**
      * This is the main program loop
      */
     while ( running )
     {
+        SDL_SetRenderDrawColor( renderer, 0xff, 0xff, 0xff, 0xff );
         SDL_RenderDrawRect( renderer, &box );
 
         // Typically these will be the last actions in each loop
